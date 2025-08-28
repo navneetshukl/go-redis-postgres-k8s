981f3e407da36ca71e291a1784745477d21ac3fc
navneetshukl <navneetshukla824@gmail.com>
2025-08-28 09:36:42 +0530

    some changes

--- main.go
+++ main.go
@@ -4,15 +4,26 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/lib/pq"
 	"github.com/redis/go-redis/v9"
 )
 
 func main() {
+	// Get environment variables with defaults
+	postgresHost := getEnv("POSTGRES_HOST", "postgres-service")
+	postgresPort := getEnv("POSTGRES_PORT", "5432")
+	postgresUser := getEnv("POSTGRES_USER", "postgres")
+	postgresPassword := getEnv("POSTGRES_PASSWORD", "password")
+	postgresDB := getEnv("POSTGRES_DB", "testdb")
+	redisHost := getEnv("REDIS_HOST", "redis-service")
+	redisPort := getEnv("REDIS_PORT", "6379")
+
 	// Connect to Postgres
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		"postgres", 5432, "postgres", "password", "testdb")
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		postgresHost, postgresPort, postgresUser, postgresPassword, postgresDB)
+	
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
@@ -27,9 +38,9 @@ func main() {
 
 	// Connect to Redis
 	client := redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
+		Addr:     fmt.Sprintf("%s:%s", redisHost, redisPort),
+		Password: "",
+		DB:       0,
 	})
 
 	pong, err := client.Ping(context.Background()).Result()
@@ -38,4 +49,11 @@ func main() {
 	}
 	fmt.Println(pong)
 	fmt.Println("Successfully connected to Redis!")
+}
+
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
 }
